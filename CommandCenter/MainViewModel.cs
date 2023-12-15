0e8e108a212fad68992c315440b9bc6596fbcad4
Thomas <Schlichting25@gmail.com>
2023-12-15 10:36:36 +0100

    Projektdateien hinzufügen.

---
+++ CommandCenter/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RoboDk;
+using RoboDk.API;
+
+namespace CommandCenter
+{
+    internal class MainViewModel
+    {
+        private RoboDK robodk;
+
+        public MainViewModel() 
+        {
+             robodk = new RoboDK();
+        
+        }
+    }
+}
